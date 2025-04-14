7bf6f848997176fe84f79c3a646958e67e3e3664
unknown <rxtmark@gmail.com>
2025-04-14 12:13:06 +0300

    [LPC-4] Installed and set up git hooks

---
+++ scripts/format-commit-msg.mjs
@@ -0,0 +1,17 @@
+import { execSync } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
+
+const commitMsgFile = process.argv[2];
+
+const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+
+const ticketPrefixMatch = branchName.match(/(LPC-\d+)/);
+
+if (ticketPrefixMatch) {
+  const ticketPrefix = `[${ticketPrefixMatch[1]}] `;
+  const commitMessage = readFileSync(commitMsgFile, 'utf8');
+
+  if (!commitMessage.startsWith(ticketPrefix)) {
+    writeFileSync(commitMsgFile, ticketPrefix + commitMessage);
+  }
+}
