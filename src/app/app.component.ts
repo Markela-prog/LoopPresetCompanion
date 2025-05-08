13f94a4875117f132cfdee3dc31abcb32e44f6e5
Markela-prog <rxtmark@gmail.com>
2025-05-08 18:46:09 +0300

    [LPC-16] Added a header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'my-presets',
+    component: AppComponent,
+  },
+  {
+    path: 'explore',
+    component: AppComponent,
+  },
+];
