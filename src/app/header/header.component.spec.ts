016bd874fa5b764ffa13bfe1a65f0c8634fe4971
Markela-prog <rxtmark@gmail.com>
2025-05-13 18:10:15 +0300

    [LPC-16] Fixed test crash issue

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { AuthGoogleServiceMock } from '../mocks/auth-google-service.mock';
 import { AuthGoogleService } from '../auth/auth-google.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -15,6 +17,14 @@ describe('HeaderComponent', () => {
         {
           provide: AuthGoogleService,
           useValue: AuthGoogleServiceMock,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            queryParams: of({}),
+            snapshot: {},
+          },
         },
       ],
     }).compileComponents();
