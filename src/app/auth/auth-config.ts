12ba38ba343a6d4e1224de3d0e8093f2aee5c442
Markela-prog <rxtmark@gmail.com>
2025-05-20 13:54:57 +0300

    [LPC-16] Moved client id to enviroment folder

--- src/app/auth/auth-config.ts
+++ src/app/auth/auth-config.ts
@@ -1,9 +1,10 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
+import { environment } from '../../enviroments/enviroment';
 
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
   redirectUri: window.location.origin,
-  clientId: '662482062057-cffcj7b97bscd0lqc121fq2gg1nenpij.apps.googleusercontent.com',
+  clientId: environment.googleClientId,
   scope: 'openid profile email',
   strictDiscoveryDocumentValidation: false,
 };
