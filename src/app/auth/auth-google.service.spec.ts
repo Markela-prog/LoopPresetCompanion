05ff4b36cff7a661542eea1bf438a34b1efa4c1c
Markela-prog <rxtmark@gmail.com>
2025-05-20 19:13:53 +0300

    [LPC-16] Fixed test issue

--- src/app/auth/auth-google.service.spec.ts
+++ src/app/auth/auth-google.service.spec.ts
@@ -16,6 +16,7 @@ describe('AuthGoogleService', () => {
             loadDiscoveryDocumentAndTryLogin: () => Promise.resolve(),
             hasValidIdToken: () => true,
             getIdentityClaims: () => ({}),
+            setStorage: () => {},
           },
         },
       ],
