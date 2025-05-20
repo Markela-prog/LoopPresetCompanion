8f805fdfb2b1ff2d3761c0d2f2cedc7e39d9bd20
Markela-prog <rxtmark@gmail.com>
2025-05-20 19:12:40 +0300

    [LPC-16] Added tab sync

--- src/app/auth/auth-google.service.ts
+++ src/app/auth/auth-google.service.ts
@@ -10,23 +10,45 @@ export class AuthGoogleService {
 
   constructor() {
     this.initConfiguration();
+    this.listenToStorageChanges();
+  }
+
+  private listenToStorageChanges() {
+    window.addEventListener('storage', (event) => {
+      if (
+        event.key === 'access_token' ||
+        event.key === 'id_token' ||
+        event.key === 'oauth2_test_storage'
+      ) {
+        this.oAuthService.loadDiscoveryDocumentAndTryLogin().then(() => {
+          this.updateProfile();
+        });
+      }
+    });
   }
 
   private initConfiguration() {
     this.oAuthService.configure(authConfig);
+    this.oAuthService.setStorage(localStorage);
     this.oAuthService.loadDiscoveryDocumentAndTryLogin().then(() => {
-      if (this.oAuthService.hasValidIdToken()) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const claims = this.oAuthService.getIdentityClaims() as any;
-        if (claims) {
-          this.profile.set({
-            name: claims.name,
-            email: claims.email,
-            picture: claims.picture,
-          });
-        }
-      }
+      this.updateProfile();
     });
+  }
+
+  private updateProfile() {
+    if (this.oAuthService.hasValidIdToken()) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const claims = this.oAuthService.getIdentityClaims() as any;
+      if (claims) {
+        this.profile.set({
+          name: claims.name,
+          email: claims.email,
+          picture: claims.picture,
+        });
+      }
+    } else {
+      this.profile.set(null);
+    }
   }
 
   login() {
