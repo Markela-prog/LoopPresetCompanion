01a868fbdd3619ab4368029a848d9e568c9aa0e7
Markela-prog <rxtmark@gmail.com>
2025-06-03 15:11:07 +0300

    [LPC-16] Removed standalone true

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,7 +5,6 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
   imports: [MatButtonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
